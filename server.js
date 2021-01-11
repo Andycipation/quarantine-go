56f15d743b84d9605391613a47290866c8144810
Andrew Dong <dongandrew99@gmail.com>
2021-01-11 16:15:02 -0500

    server

---
+++ server.js
@@ -0,0 +1,37 @@
+/*
+Server code.
+*/
+
+const express = require('express');
+const app = express();
+app.use(express.static('docs'));
+
+// app.get('*', (req, res) => {
+//   res.redirect('https://andrewdongandy.github.io/quarantine-go/');
+// });
+
+const PORT = process.env.PORT || 8000;
+const server = app.listen(PORT);
+
+const socket_io = require('socket.io');
+const io = socket_io(server);
+
+// assume only 2 players for now
+const ids = new Set();
+
+io.on('connect', socket => {
+  console.log('new socket with id ' + socket.id);
+  socket.emit('player_assignment', { playerNumber: ids.size });
+  console.log('sent player assignment ' + ids.size);
+  ids.add(socket.id);
+  
+  socket.on('move', data => {
+    // console.log(data);
+    socket.broadcast.emit('opponent_moved', data);
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('socket id ' + socket.id + ' disconnected');
+    ids.delete(socket.id);
+  })
+});
